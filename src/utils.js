d07726f15401e669f624065125c2b5e54d8a3acd
dagda25 <dagda25@gmail.com>
2021-02-13 10:19:37 +0300

    исправил недочеты первого модуля

--- src/utils.js
+++ src/utils.js
@@ -1,12 +1,12 @@
 'use strict';
 
-module.exports.getRandomInt = (min, max) => {
+const getRandomInt = (min, max) => {
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 
-module.exports.shuffle = (array) => {
+const shuffle = (array) => {
   for (let i = array.length - 1; i > 0; i--) {
     const randomPosition = Math.floor(Math.random() * i);
     [array[i], array[randomPosition]] = [array[randomPosition], array[i]];
@@ -14,3 +14,5 @@ module.exports.shuffle = (array) => {
 
   return array;
 };
+
+module.exports = {getRandomInt, shuffle};
